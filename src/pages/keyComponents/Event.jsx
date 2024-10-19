e0d92a4fa4800e8f7290f196e2641953f1abe4e4
Jay <aphfos.jjw@gmail.com>
2024-10-19 22:04:29 +0900

    10.19 프론트 6차 작업 - 일정관리 UI 형성

--- src/pages/keyComponents/Event.jsx
+++ src/pages/keyComponents/Event.jsx
@@ -1,10 +1,30 @@
 import Typography from '@mui/material/Typography';
 import MainCard from 'components/MainCard';
+import React, { Component } from 'react';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
 
-export default function Event() {
+import './Event.css';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+const localizer = momentLocalizer(moment);
+
+const state = {
+  events: [
+    {
+      start: moment().toDate(),
+      end: moment().add(1, 'days').toDate(),
+      title: 'Some title'
+    }
+  ]
+};
+
+export default function Event(props) {
   return (
-    <MainCard title="일정관리">
-      <Typography variant="body2">일정관리</Typography>
+    <MainCard title="">
+      <div className="App">
+        <Calendar localizer={localizer} defaultDate={new Date()} defaultView="month" events={state.events} style={{ height: '100vh' }} />
+      </div>
     </MainCard>
   );
 }
