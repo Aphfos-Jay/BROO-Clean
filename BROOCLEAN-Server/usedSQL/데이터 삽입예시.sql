1b46bd802edd0bae4430362ba4996a80143b9ee5
SG <sg_youtube@naver.com>
2024-11-25 14:49:36 +0900

    test

--- BROOCLEAN-Server/usedSQL/데이터 삽입예시.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO reports (subject, description, status, image, createdDate, mobile, email, location)
-values('쓰레기 투기 의심 신고','안녕하세요. \n 쓰레기 투기가 의심되어 신고하였습니다. 여기 확인 부탁드립니다. ',0,'',date('2024-11-08'),'010-1234-5678','testEmail@naver.com',point(37.405184,129.209373));
