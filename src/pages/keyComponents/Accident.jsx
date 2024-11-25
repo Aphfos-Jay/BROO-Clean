208ec7cb7962d13aa2dbcb7adf969fa0cc31319f
Choi <jiyong0808@gmail.com>
2024-11-25 23:13:56 +0900

    해양사고 통계 - 지도 html 파일 및 불러오기

--- src/pages/keyComponents/Accident.jsx
+++ src/pages/keyComponents/Accident.jsx
@@ -1,13 +1,13 @@
 import React, { useState } from 'react';
-import MainCard from '../../components/MainCard'; // 정확한 경로로 수정
+import MainCard from '../../components/MainCard';
 
 export default function Accident() {
   const [selectedYear, setSelectedYear] = useState('2022');
 
   // 연도별 HTML 파일 경로 매핑
   const fileMap = {
-    '2022': '/accident_visualization_2022.html', // public 폴더 기준 경로
-    '2023': '/accident_visualization_2023.html', // public 폴더 기준 경로
+    '2022': '/accident_visualization_2022.html', //public 폴더 기준 경로
+    '2023': '/accident_visualization_2023.html', //public 폴더 기준 경로
   };
 
   return (
@@ -32,8 +32,8 @@ export default function Accident() {
 
       {/* iframe으로 선택된 연도의 지도 HTML 파일 출력 */}
       <iframe
-        src={fileMap[selectedYear]} // 선택된 연도에 따라 src 변경
-        style={{ width: '100%', height: '700px', border: 'none', marginTop: '20px' }}
+        src={fileMap[selectedYear]}
+        style={{ width: '100%', height: '850px', border: 'none' }}
         title={`Marine Accident Visualization ${selectedYear}`}
       ></iframe>
     </MainCard>
